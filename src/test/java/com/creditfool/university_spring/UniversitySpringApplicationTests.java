9c931a7c98b44204ad836102ef9e313b639caf2f
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-09-06 20:57:38 +0700

    project init

---
+++ src/test/java/com/creditfool/university_spring/UniversitySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditfool.university_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
