1e2d80bc6c8f138d9a5dbbc00a16ccf3818d5f3c
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-09-07 16:24:08 +0700

    add method find by nip in teachre repository

--- src/main/java/com/creditfool/university_spring/repository/TeacherRepository.java
+++ src/main/java/com/creditfool/university_spring/repository/TeacherRepository.java
@@ -2,8 +2,10 @@ package com.creditfool.university_spring.repository;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.creditfool.university_spring.entity.Teacher;
 
@@ -12,4 +14,7 @@ public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
     List<Teacher> findByIsActive(Boolean isActive);
 
     Teacher findByIdAndIsActive(UUID id, Boolean isActive);
+
+    @Query("SELECT t FROM Teacher t WHERE t.nip = :nip AND t.isActive = true")
+    Optional<Teacher> findByNip(String nip);
 }
