6be03edf7fa9381cb879e7acfa5aefa4f3d79adc
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-09-09 17:07:07 +0700

    add spring actuator to dependency

--- src/main/java/com/creditfool/university_spring/util/customvalidation/PhoneNumberValidator.java
+++ src/main/java/com/creditfool/university_spring/util/customvalidation/PhoneNumberValidator.java
@@ -5,6 +5,14 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class PhoneNumberValidator implements ConstraintValidator<PhoneNumber, String> {
 
+    // /**
+    //  * Checks if the given string is a valid phone number.
+    //  * 
+    //  * @param value the phone number to be validated
+    //  * @param context the context in which the constraint is evaluated
+    //  * 
+    //  * @return true if the phone number is valid, false otherwise
+    //  */
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null) {
