f50449d7a57b764498e0fa29b55ab2522b141729
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-09-16 12:23:32 +0700

    Add @Query annotations to StudentRepository and TeacherRepository for precision result

--- src/main/java/com/creditfool/university_spring/repository/StudentRepository.java
+++ src/main/java/com/creditfool/university_spring/repository/StudentRepository.java
@@ -2,9 +2,13 @@ package com.creditfool.university_spring.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+
 import com.creditfool.university_spring.entity.Student;
 
 public interface StudentRepository extends CommonMethodRepository<Student> {
+
+    @Query("SELECT s FROM Student s WHERE (s.email = :email OR s.phone = :phone OR s.nim = :nim) AND s.deletedAt is null")
     List<Student> findAllByEmailIgnoreCaseOrPhoneOrNimAndDeletedAtIsNull(String email, String phone, String nim);
 
 }
--- src/main/java/com/creditfool/university_spring/repository/TeacherRepository.java
+++ src/main/java/com/creditfool/university_spring/repository/TeacherRepository.java
@@ -2,8 +2,12 @@ package com.creditfool.university_spring.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+
 import com.creditfool.university_spring.entity.Teacher;
 
 public interface TeacherRepository extends CommonMethodRepository<Teacher> {
+
+    @Query("SELECT t FROM Teacher t WHERE (t.email = :email OR t.phone = :phone OR t.nip = :nip) AND t.deletedAt is null")
     List<Teacher> findAllByEmailIgnoreCaseOrPhoneOrNipAndDeletedAtIsNull(String email, String phone, String nip);
 }
