5171b09459c55e10500432a1bc731ce6988e5aff
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-09-16 18:05:00 +0700

    Refactor indentation in `StudentDetailResponse` record for consistency

--- src/main/java/com/creditfool/university_spring/dto/response/StudentDetailResponse.java
+++ src/main/java/com/creditfool/university_spring/dto/response/StudentDetailResponse.java
@@ -1,13 +1,13 @@
 package com.creditfool.university_spring.dto.response;
 
 public record StudentDetailResponse(
-                String id,
-                String firstName,
-                String lastName,
-                String nim,
-                String address,
-                String phone,
-                String email
+        String id,
+        String firstName,
+        String lastName,
+        String nim,
+        String address,
+        String phone,
+        String email
 
 ) {
 }
